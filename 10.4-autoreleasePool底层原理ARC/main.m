9c3d3e4f9e6f5b804a07d21ef0bcece56bd53797
刘光强 <1126756952@qq.com>
2020-02-15 22:28:19 +0800

    Initial Commit

---
+++ 10.4-autoreleasePool底层原理ARC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  10.4-autoreleasePool底层原理ARC
+//
+//  Created by 刘光强 on 2020/2/15.
+//  Copyright © 2020 guangqiang.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
